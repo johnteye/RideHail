e013f282cb34dab3d47848d4399bf3747ad05c6a
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-11-19 19:50:54 +0000

    Generate UserId

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import threading
 import time
 from twilio.rest import Client
+import uuid
 
 from decouple import config
 
@@ -207,7 +208,8 @@ def sms_reply():
             
     else:
         # New user flow
-        user = User(phone_number=from_number, state='awaiting_name')
+        user_id = uuid.uuid4().hex 
+        user = User(id=user_id, phone_number=from_number, state='awaiting_name')
         session.add(user)
         session.commit()
         message.body("Welcome to Ride-Hailing App! Please enter your full name:")
